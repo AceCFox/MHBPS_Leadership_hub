1e9d285ec863add710d5bd4089bd478944fb1cf8
GayofReckoning <44708780+GayofReckoning@users.noreply.github.com>
2023-06-04 12:14:42 -0500

    initial commit - added link hub

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="logo-zone">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>2023 MHB Leadership Hub</h1>
+        </div>
+        <h2>Quick links:</h2>
+        <ul>
+          <li>
+           <a href="https://forms.gle/ZV5Xvgz4PANJzhQW8">Record attendance</a>
+          </li>
+          <li>
+            <a href="https://docs.google.com/spreadsheets/d/1Y-ZAmVDQ7V-3OSt-miCrIgZgkw8vg61-2xo_zy_9fGg/edit?usp=sharing">
+              View skater attendance
+            </a>
+          </li>
+          <li>
+           <a href="https://docs.google.com/document/d/1-aSGOohYmIzcBq6V9T9jZPQVL5GE0uLoTYOrbU-UM3w/edit?usp=sharing">
+              Attendance policy + periods
+              </a>
+          </li>
+          <li>
+           <a href="https://forms.gle/RnCoCBrX3ASfiXHt8">
+              Record absenses
+            </a>
+          </li>
+          <li>
+           <a href="https://docs.google.com/document/d/1CE3V7HzhCFXQMoZSVmid3woeG4RGdM7zueg5dUtjPfw/edit?usp=drive_link">
+              Practice squad and BA/SB crossover policy
+            </a>
+          </li>
+          <li>
+           <a href="https://docs.google.com/spreadsheets/d/157oqP3dRCYEjxflS_FDbXCdXH3Pd0jZeS71SF40T3pQ/edit#gid=0">
+              Program charter and skater goals
+            </a>
+          </li>
+        </ul>
       </header>
     </div>
   );
